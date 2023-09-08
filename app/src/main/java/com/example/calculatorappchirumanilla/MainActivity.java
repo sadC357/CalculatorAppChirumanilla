55c0f045f0f963ed74e01e565843cd576849bb9c
sadC357 <sadhikac357@gmail.com>
2023-09-08 12:55:21 -0500

    fixed issue with the images and fixed issues with buttons, started on java functionality

--- app/src/main/java/com/example/calculatorappchirumanilla/MainActivity.java
+++ app/src/main/java/com/example/calculatorappchirumanilla/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.calculatorappchirumanilla;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int input_one;
+    private int input_two;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public double addition(){
+        return (double) input_one+input_two;
+    }
+
+    public double subtraction(){
+        return (double) input_one-input_two;
+    }
+
+    public double division(){
+        return (double) input_one/input_two;
+    }
+
 }
