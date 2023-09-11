e13cce577586aceba8de4f469413e172364300f9
sadC357 <sadhikac357@gmail.com>
2023-09-11 12:54:21 -0500

    worked on java functionality

--- app/src/main/java/com/example/calculatorappchirumanilla/MainActivity.java
+++ app/src/main/java/com/example/calculatorappchirumanilla/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.calculatorappchirumanilla;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -10,24 +11,53 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView input;
-    private String in_string="";
+    private Button button;
+    private String buttonText="";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView in1_TV=findViewById(R.id.textView_input);
     }
 
-    public void setInputs(String val){
-        in_string=in_string+val;
-        input.setText(in_string+val);
+
+    public void onClick0(View view){
+        Button button=findViewById(R.id.button_num0);
+        buttonText+=button.getText().toString();
     }
 
-    public void onClick(View view){
-        String buttonText=((Button)view).getText().toString();
-        setInputs(buttonText);
+    public void onClick1(View view){
+        Button button=findViewById(R.id.button_num1);
+        buttonText+=button.getText().toString();
     }
 
+    public void onClick2(View view3){
+        button=findViewById(R.id.button_num2);
+        buttonText+=button.getText().toString();
+        Log.i("ch_sad",buttonText);
+    }
+
+    public void onClick3(View view){
+        Button button=findViewById(R.id.button_num3);
+        buttonText+=button.getText().toString();
+    }
+
+    public void onClick4(View view){
+        Button button=findViewById(R.id.button_num4);
+        buttonText+=button.getText().toString();
+    }
+
+    public void onClickPlus(View view){
+        Button button=findViewById(R.id.button_plus);
+        buttonText+=button.getText().toString();
+    }
+
+    public void onClickEquals(View view){
+        Button button=findViewById(R.id.button_equals);
+        buttonText+=button.getText().toString();
+        Log.i("ch_sad",buttonText);
+    }
 
 }
