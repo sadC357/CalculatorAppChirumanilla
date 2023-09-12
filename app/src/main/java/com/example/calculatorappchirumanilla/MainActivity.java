3a79e93e9b5a5e13c76e73c6878fe2b2fda18328
sadC357 <sadhikac357@gmail.com>
2023-09-11 23:38:39 -0500

    finished code functionality, added in citations

--- app/src/main/java/com/example/calculatorappchirumanilla/MainActivity.java
+++ app/src/main/java/com/example/calculatorappchirumanilla/MainActivity.java
@@ -1,63 +1,114 @@
 package com.example.calculatorappchirumanilla;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button button;
-    private String buttonText="";
+    private TextView in;
+    private String cur="";
+    private double val_1=0;
+    private String func="";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView in1_TV=findViewById(R.id.textView_input);
+        in=findViewById(R.id.textView_display);
+        reset();
     }
 
-
-    public void onClick0(View view){
-        Button button=findViewById(R.id.button_num0);
-        buttonText+=button.getText().toString();
+    public void getNum(View input) {
+        String num;
+        Button button=(Button)input;
+        num=button.getText().toString();
+        cur+=num;
+        reset();
     }
 
-    public void onClick1(View view){
-        Button button=findViewById(R.id.button_num1);
-        buttonText+=button.getText().toString();
+    public void getFunction(View input) {
+        Button button=(Button)input;
+        func=button.getText().toString();
+
+        /*https://www.geeksforgeeks.org/convert-string-to-double-in-java/
+        parseDouble() converts the String value into a double value
+        */
+        val_1=Double.parseDouble(cur);
+        cur="";
     }
 
-    public void onClick2(View view3){
-        button=findViewById(R.id.button_num2);
-        buttonText+=button.getText().toString();
-        Log.i("ch_sad",buttonText);
+    public void clear(View input) {
+        cur="";
+        func="";
+        reset();
     }
 
-    public void onClick3(View view){
-        Button button=findViewById(R.id.button_num3);
-        buttonText+=button.getText().toString();
+    public void equalsTo(View input) {
+        /* https://www.w3schools.com/java/ref_string_isempty.asp
+        isEmpty() checks to see if a string is empty or not
+        and returns true if it is empty
+         */
+        if (!cur.isEmpty() && !func.equals("%")) {
+                /*https://www.geeksforgeeks.org/convert-string-to-double-in-java/
+                parseDouble() converts the String value into a double value
+                 */
+                double val_2=Double.parseDouble(cur);
+                double out_1=0.0;
+                if (func.equals("+")) {
+                    out_1=val_1+val_2;
+                } else if (func.equals("-")) {
+                    out_1=val_1-val_2;
+                } else if (func.equals("*")) {
+                    out_1=val_1*val_2;
+                } else if (func.equals("/")) {
+                    if (val_2!=0) {
+                        out_1=val_1/val_2;
+                    } else {
+                        cur="Error: Can't divide a number by zero";
+                        reset();
+                        return;
+                    }
+                } else if (func.equals("^")) {
+                    /*https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/pow
+                    Math.pow() returns the first value to the power of the second value
+                     */
+                    out_1=Math.pow(val_1,val_2);
+                }
+
+                /*https://www.tutorialspoint.com/java/number_valueof.htm
+                valueOf() makes the double value into a String
+                in order to store it in a string variable
+                 */
+                cur=String.valueOf(out_1);
+                func="";
+                reset();
+        }
+        if(func.equals("%")) {
+            double out_2=0.0;
+            out_2=val_1/100;
+            /*https://www.tutorialspoint.com/java/number_valueof.htm
+            valueOf() makes the double value into a String
+            in order to store it in a string variable
+             */
+            cur=String.valueOf(out_2);
+            func="";
+            reset();
+        }
     }
 
-    public void onClick4(View view){
-        Button button=findViewById(R.id.button_num4);
-        buttonText+=button.getText().toString();
+    private void reset() {
+        /* https://www.w3schools.com/java/ref_string_isempty.asp
+        isEmpty() checks to see if a string is empty or not
+        and returns true if it is empty
+         */
+        if(cur.isEmpty()){
+            in.setText("0");
+        }else{
+            in.setText(cur);
+        }
     }
-
-    public void onClickPlus(View view){
-        Button button=findViewById(R.id.button_plus);
-        buttonText+=button.getText().toString();
-    }
-
-    public void onClickEquals(View view){
-        Button button=findViewById(R.id.button_equals);
-        buttonText+=button.getText().toString();
-        Log.i("ch_sad",buttonText);
-    }
-
 }
